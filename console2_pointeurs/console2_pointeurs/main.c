ec7431efec3d91c78bf551983eaf036d424e6494
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-03-19 18:29:01 +0100

    n

--- console2_pointeurs/console2_pointeurs/main.c
+++ console2_pointeurs/console2_pointeurs/main.c
@@ -7,32 +7,21 @@
 
 #include <stdio.h>
 
-void decoupeMinutes(int* pointeurHeures, int *pointeurMinutes);
+void decoupeMinutes(int* pointeurHeures, int* pointeurMinutes);
 
-void triplePointeur(int *pointeurNombre);
-int main(int argc, const char * argv[]) {
-    /*
-    int age=20;
-    int *pointeurAge=&age;
-    
-    printf("%d\n", pointeurAge);
-    printf("%d\n", *pointeurAge);
-    */
-    /*
-    int nombre=5;
-    triplePointeur(&nombre);
-    printf("%d\n", nombre);
-    */
-    int heures=0, minutes=90
+int main(int argc, char *argv[])
+{
+    int heures = 0, minutes = 90;
+
     decoupeMinutes(&heures, &minutes);
-    printf("")
+
+    printf("%d heures et %d minutes", heures, minutes);
+
     return 0;
 }
 
-void decoupeMinutes(int* pointeurHeures, int* pointeurMinutes){
+void decoupeMinutes(int* pointeurHeures, int* pointeurMinutes)
+{
     *pointeurHeures = *pointeurMinutes / 60;
-    *pointeurMinutes = *pointeurHeures * 60;
-}
-void triplePointeur(int *pointeurNombre){
-    *pointeurNombre *= 3;
+    *pointeurMinutes = *pointeurMinutes % 60;
 }
